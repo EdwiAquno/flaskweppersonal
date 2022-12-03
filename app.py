ef8f18f63386069faf5d016bf5e610ae6d43609b
Edwin <edwin.nol2.123@gmail.com>
2022-12-03 00:10:42 -0600

    bluepri Error Pages con errorhandler

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask, render_template, request,redirect,url_for
 
 
+from flask import Flask
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from error_pages.handlers import error_pages
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
+app.config['ENV'] = 'development'
 
 
 
 
 
-@app.errorhandler(404)
-def page_error_not_found(e):
-    return render_template('error/404.html'), 404
 
 ################# Apps ##################
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
+app.register_blueprint(error_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
