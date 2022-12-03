30ba6b5ada8a570af53d6c2a3de2983798536495
Edwin <edwin.nol2.123@gmail.com>
2022-12-02 23:14:45 -0600

    bluepri home

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
 from flask import Flask, render_template, request,redirect,url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms import (StringField, PasswordField, 
+                    SubmitField, EmailField, 
+                    IntegerField, RadioField, 
+                    SelectField, TextAreaField)
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+from home.views import home_blueprint
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
-######## Rutas publicas
-
-@app.route('/')
-def index():
-      return render_template('public/index.html')
-
-@app.route('/about')
-def about():
-    return render_template('public/about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('public/contact.html')    
-
-@app.route('/portfolio')    
-def portfolio():
-    projects= [
-        { 
-            'name':'Primer proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/home-bg.jpg',
-            'url': 'http://google.com'
-
-         },
-         {
-            'name':'Segundo proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-            'image':'img/about-bg.jpg',
-            'url': 'http://xataka.com'
-         }
-    ]
-    return render_template('public/portfolio.html', projects=projects)
 
 
 ##########formularos flask_wtf############
@@ -47,6 +22,16 @@ class LoginForm(FlaskForm):
     email = EmailField('Correo', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
+
+class RegisterForm(FlaskForm):
+    name = StringField('Nombre')
+    last_name = StringField('Apellidos')
+    email = EmailField('Correo')
+    password = PasswordField('Contraseña')
+    phone = IntegerField('Teléfono')
+    is_married = RadioField('Estado Civil', choices=[('True', 'Casado'), ('False', 'Soltero')])
+    gender = SelectField('Genero', choices=[('male', 'Masculino'), ('female', 'Femenino'), ('other', 'Otro')])
+    submit = SubmitField('Registrar')    
 
 ##########Rutas###########
 
@@ -68,23 +53,19 @@ def login():
 
 @app.route('/auth/register')    
 def register():
-    return render_template('auth/login.html')
-    form = LoginForm()
-    return render_template('auth/login.html', form=form)
+    form = RegisterForm()
 
-@app.route('/welcome', methods=['GET', 'POST'])    
-def welcome(form):
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        
+    return render_template('auth/register.html', form=form)
 
-        return render_template('admin/index.html', email=email)
-    return redirect(url_for("login"))
+
+
 
 @app.errorhandler(404)
 def page_error_not_found(e):
     return render_template('error/404.html'), 404
+
+################# Apps ##################
+app.register_blueprint(home_blueprint)
 
 
 if __name__ == '__main__':
